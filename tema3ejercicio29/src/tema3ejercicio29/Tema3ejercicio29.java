c824cd2f5b4d23bdbe7ed2c0559256f8e087badd
mariete54 <mariohernandez2112003@gmail.com>
2026-01-29 20:20:47 +0100

    Add comment for Git test in Tema3ejercicio29.java

--- tema3ejercicio29/src/tema3ejercicio29/Tema3ejercicio29.java
+++ tema3ejercicio29/src/tema3ejercicio29/Tema3ejercicio29.java
@@ -6,7 +6,7 @@ public class Tema3ejercicio29 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//Cuartalinea git
 		
 		//DWADWADWADAWDWAD
 //Github primera prueba
